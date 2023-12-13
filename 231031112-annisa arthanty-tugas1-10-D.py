003ca038f10a2f12417b8d5cbc99013ab822553c
annisaarthanty <144190159+annisaarthanty@users.noreply.github.com>
2023-12-13 19:13:55 +0800

    Delete 231031112-annisa arthanty-tugas1-10-D.py

--- 231031112-annisa arthanty-tugas1-10-D.py
+++
@@ -1,9 +0,0 @@
-
-Nim = 231031112
-
-bilangan = int(input("Masukkan bilangan: "))
-               
-if bilangan % 2 == 0:
-    print("Bilangan", bilangan, "adalah bilangan genap.")
-else:
-    print("Bilangan", bilangan, "adalah bilangan ganjil.")
